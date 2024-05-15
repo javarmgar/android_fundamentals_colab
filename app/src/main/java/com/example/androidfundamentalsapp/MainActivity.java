2aed24f71824947b26a47a9b7a97898040024802
vazch <luis.vazquez.ch08@gmail.com>
2024-05-14 20:22:14 -0600

    Best commit ever

--- app/src/main/java/com/example/androidfundamentalsapp/MainActivity.java
+++ app/src/main/java/com/example/androidfundamentalsapp/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // CHANGE FROM JAVI
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
+//@TODO @javarmgar
+// finish the project
