c955c9f1770cbe54caffd5708a4cebbd8494ac6d
Javier Armenta <j.armentagarcia@fetchrewards.com>
2024-05-16 20:16:24 -0600

    extension functions, lambda functions, scoped functions. setContentView behind scenes, val and var, generics peek. activity Hierarchy

---
+++ app/src/main/java/com/example/androidfundamentalsapp/EmailSenderActivity.kt
@@ -0,0 +1,73 @@
+package com.example.androidfundamentalsapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.Stack
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+class EmailSenderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_sender)
+
+        val textMessage = "javo"
+        val sendIntent = Intent()
+
+//        sendIntent.action = Intent.ACTION_SEND
+//        sendIntent.type = "text/plain"
+//        sendIntent.putExtra(Intent.EXTRA_TEXT,textMessage)
+
+        val lambda: Intent.() -> Unit = {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT,textMessage)
+        }
+        sendIntent.apply(lambda)
+
+        sendIntent.apply{
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT,textMessage)
+        }
+
+        val printNumber: () -> Unit = {
+
+            -> println("havs")
+        }
+        textMessage.print()
+
+        val pila: Stack<Int> = Stack<Int>()
+        pila.push(2)
+        pila.push(3)
+        pila.push(1)
+
+    }
+
+    fun String.printStandardFunction() {
+        println(this)
+    }
+
+    fun String.printLambdaFunction() = {
+        ->
+        println(this)
+    }
+    val printLambda: String.() -> Unit = {
+        ->
+        println(this)
+
+    }
+    fun String.print() = printLambda
+
+    fun standardMethod (){
+        println("javi")
+    }
+
+    fun lambdaMethod() = {
+        println("Jaby")
+        println("Jaby")
+
+    }
+}
