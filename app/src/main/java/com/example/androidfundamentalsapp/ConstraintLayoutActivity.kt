b9611de8f15b938e48ca1c07c9098844a2d5d405
vazch <luis.vazquez.ch08@gmail.com>
2024-06-17 20:47:42 -0600

    Constraint Layout features

--- app/src/main/java/com/example/androidfundamentalsapp/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/example/androidfundamentalsapp/ConstraintLayoutActivity.kt
@@ -1,12 +1,17 @@
 package com.example.androidfundamentalsapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ConstraintLayoutActivity : AppCompatActivity() {
+    private lateinit var visibilityView1: View
+    private lateinit var visibilityButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,23 @@ class ConstraintLayoutActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // VISIBILITY EXAMPLE
+
+        var i = 0
+        val visibilityArray = listOf(
+            View.INVISIBLE,
+            View.VISIBLE,
+            View.GONE,
+            View.INVISIBLE
+        )
+        visibilityButton = findViewById<Button>(R.id.visibility_button)
+        visibilityView1 = findViewById<View>(R.id.visibility_view)
+        visibilityButton.setOnClickListener {
+            visibilityView1.visibility = visibilityArray[i]
+            i++
+            if(i == 4) i = 0
         }
     }
 }
